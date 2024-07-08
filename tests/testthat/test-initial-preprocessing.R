27a3ceecba5df69472d9798de07aa929e99b6d7d
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2024-07-08 18:56:22 +0200

    Add sample files and adequate tests

--- tests/testthat/test-initial-preprocessing.R
+++ tests/testthat/test-initial-preprocessing.R
@@ -1,35 +1,45 @@
-test_that("blank adjustment works", {
-  kenya_filepath <- system.file("extdata", "kenya.csv", package = "PvSTATEM", mustWork = TRUE)
+library(testthat)
 
-  plate <- read_data(kenya_filepath, verbose=FALSE)
+test_that("blank adjustment", {
+  plate_file <- system.file("extdata", "random.csv", package = "PvSTATEM", mustWork = TRUE)
 
-  initial_mfi_value = plate$samples[[1]]$data["Net MFI", "Etramp5_ag1"]
-  expect_equal(initial_mfi_value, 5595.5)
+  plate <- read_data(plate_file, verbose = FALSE)
+
+  initial_mfi_value <- plate$samples[[2]]$data["Median", "Etramp5_ag1"]
+  expect_equal(initial_mfi_value, 1292)
 
   plate$blank_adjustment(inplace = T, method = "avg")
 
-  recalculated_mfi_value = plate$samples[[1]]$data["Net MFI", "Etramp5_ag1"]
-  expect_equal(recalculated_mfi_value, 5545.3333)
+  recalculated_mfi_value <- plate$samples[[2]]$data["Median", "Etramp5_ag1"]
+  expect_equal(recalculated_mfi_value, 1272)
 })
 
 test_that("plotting standard curves work", {
+  plate_file1 <- system.file("extdata", "random.csv", package = "PvSTATEM", mustWork = TRUE)
+  plate_file2 <- system.file("extdata", "random.csv", package = "PvSTATEM", mustWork = TRUE)
 
-  kenya_filepath1 <- system.file("extdata", "kenya.csv", package = "PvSTATEM", mustWork = TRUE)
-  kenya_filepath6 <- system.file("extdata", "kenya_P6.csv", package = "PvSTATEM", mustWork = TRUE)
-  kenya_filepath4 <- system.file("extdata", "kenya_P4.csv", package = "PvSTATEM", mustWork = TRUE)
-
-  plate1 <- read_data(kenya_filepath1, verbose=FALSE)
-  plate6 <- read_data(kenya_filepath6, verbose=FALSE)
-  plate4 <- read_data(kenya_filepath4, verbose=FALSE)
+  plate1 <- read_data(plate_file1, verbose = FALSE)
+  plate2 <- read_data(plate_file2, verbose = FALSE)
 
   plate1$blank_adjustment()
-  plate4$blank_adjustment()
-  plate6$blank_adjustment()
+  plate2$blank_adjustment()
 
-
-
-  plates <- list(plate1, plate4, plate6)
-
+  plates <- list(plate1, plate2)
 
   expect_error(plot_standard_curve_antibody(plates, antibody_name = "Etramp5_ag1"), NA)
+})
+
+test_that("plotting standard curves fail - different dilutions", {
+  plate_file1 <- system.file("extdata", "random.csv", package = "PvSTATEM", mustWork = TRUE)
+  plate_file2 <- system.file("extdata", "random2.csv", package = "PvSTATEM", mustWork = TRUE)
+
+  plate1 <- read_data(plate_file1, verbose = FALSE)
+  plate2 <- read_data(plate_file2, verbose = FALSE)
+
+  plate1$blank_adjustment()
+  plate2$blank_adjustment()
+
+  plates <- list(plate1, plate2)
+
+  expect_error(plot_standard_curve_antibody(plates, antibody_name = "Etramp5_ag1"))
 })
