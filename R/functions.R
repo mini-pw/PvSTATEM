5550815220646965581a83b80a2703e3df242740
nizwant <mateusznizwantowski@gmail.com>
2024-08-01 11:29:30 +0200

    removed deriving dilutions in this function

--- R/functions.R
+++ R/functions.R
@@ -1,5 +1,6 @@
 #' @description
-#' Parses the sample type based on the sample name and dilution factor
+#' Parses the sample type based on the sample name from Luminex file and
+#' the sample name from the layout file, but this one might not be provided
 #'
 #' It parses the names as follows:
 #' If `sample_name` or `sample_name_from_layout` equals to `BLANK`, `
@@ -15,48 +16,49 @@
 #' otherwise, the returned SampleType is `TEST`
 #'
 #'
-#' @param sample_name (`character(1)`)\cr
-#' The name of the sample
+#' @param sample_name (`character`)\cr
+#' Vector of sample names from Luminex file
 #'
-#' @param sample_name_from_layout (`character(1)`)\cr
-#' This param represent the name of the sample provided in the layout file.
-#' This filed may be different than `sample_name` and may contain additional
-#' information about the sample type
+#' @param sample_name_from_layout (`character`)\cr
+#' Vector of sample names from Layout file
+#' values in this vector may be different than `sample_name` and may
+#' contain additional information about the sample type like dilution
 #'
-#' @param dilution_factor (`numeric(1)`)\cr
-#' The dilution factor of the sample from the base luminex file.
-#' This parameter is ignored for now
 #'
-#' @return a new `SampleType` object
+#' @return a vector of valid sample_types strings
 #'
 #'
 #' @export
-derive_sample_type_and_dilution <- function(sample_name,
-                                         sample_name_from_layout = "",
-                                         dilution_factor = 1) {
+derive_sample_type <- function(sample_name,
+                               sample_name_from_layout = "") {
+  # handle case when sample name from layout is not provided
   if (is.null(sample_name_from_layout) || is.na(sample_name_from_layout)) {
     sample_name_from_layout <- ""
   }
 
-  blank_types <- c("BLANK", "BACKGROUND", "B")
-
-  if (sample_name %in% blank_types ||
-    sample_name_from_layout %in% blank_types) {
-    return(SampleType$new("BLANK"))
-  }
-
+  # default sample type is test
   sample_type <- "TEST"
 
+  # check if the sample is a blank
+  blank_types <- c("BLANK", "BACKGROUND", "B")
+  if (sample_name %in% blank_types ||
+    sample_name_from_layout %in% blank_types) {
+    sample_type <- "BLANK"
+  }
+
+
+  # check if the sample is a positive control
   positive_control_pattern <- c("^(P.|POS.+|B770.+|10/198.+)(1/\\d+)$")
   if (grepl(positive_control_pattern, sample_name) ||
     grepl(positive_control_pattern, sample_name_from_layout)) {
     sample_type <- "POSITIVE CONTROL"
   }
 
+
+  # check if the sample is a negative control
   negative_types <- c("NEGATIVE CONTROL", "N")
   negative_pattern <-
     "^(N..|.*\\bNEG\\b)" # check if it starts with N or contains NEG string
-
   if (sample_name %in% negative_types ||
     grepl(negative_pattern, sample_name) ||
     grepl(negative_pattern, sample_name_from_layout)) {
@@ -64,6 +66,7 @@ derive_sample_type_and_dilution <- function(sample_name,
   }
 
 
+  # check if the sample is a standard curve
   standard_curve_types <- c("STANDARD CURVE", "SC", "S") # CP3 - tanzania, PRISM - uganda, Brefet - gambia, NIBSC 10/198
   standard_curve_pattern <- "^(S_|S|S\\s|CP.+)(1/\\d+)$"
   standard_curve_loc_pattern <- "(1/\\d+)"
@@ -73,73 +76,5 @@ derive_sample_type_and_dilution <- function(sample_name,
     sample_type <- "STANDARD CURVE"
   }
 
-  if (sample_type %in% c("STANDARD CURVE", "POSITIVE CONTROL", "NEGATIVE CONTROL")) {
-    dilution_factor_pattern <- "1/\\d+"
-    match <- ""
-    if (!is.null(sample_name_from_layout) && sample_name_from_layout != "" ||
-      !is.na(sample_name_from_layout) && sample_name_from_layout != "") {
-      match <- regmatches(sample_name_from_layout, regexpr(
-        dilution_factor_pattern,
-        sample_name_from_layout
-      ))
-    } else {
-      match <- regmatches(sample_name, regexpr(
-        dilution_factor_pattern,
-        sample_name
-      ))
-    }
-    dilution_factor <- eval(parse(text = match))
-
-    if (is.null(dilution_factor)) {
-      dilution_factor <- NA # this value needs to be updated later
-    }
-
-    return(SampleType$new("STANDARD CURVE",
-      dilution_factor = dilution_factor,
-      validate_dilution = FALSE
-    ))
-  }
-
-  negative_types <- c("NEGATIVE CONTROL", "N")
-  negative_pattern <-
-    "^(N..|.*\\bNEG\\b)" # check if it starts with N or contains NEG string
-
-  if (sample_name %in% negative_types ||
-    grepl(negative_pattern, sample_name) ||
-    grepl(negative_pattern, sample_name_from_layout)) {
-    return(SampleType$new("NEGATIVE CONTROL"))
-  }
-
-
-
-  positive_control_pattern <- c("^(P.+|POS.+|CP.+)(1/\\d+)$")
-  if (grepl(positive_control_pattern, sample_name) ||
-    grepl(positive_control_pattern, sample_name_from_layout)) {
-    dilution_factor_pattern <- "1/\\d+"
-    match <- ""
-    if (!is.null(sample_name_from_layout) && sample_name_from_layout != "" ||
-      !is.na(sample_name_from_layout) && sample_name_from_layout != "") {
-      match <- regmatches(sample_name_from_layout, regexpr(
-        dilution_factor_pattern,
-        sample_name_from_layout
-      ))
-    } else {
-      match <- regmatches(sample_name, regexpr(
-        dilution_factor_pattern,
-        sample_name
-      ))
-    }
-    dilution_factor <- eval(parse(text = match))
-
-    if (is.null(dilution_factor)) {
-      dilution_factor <- NA # this value needs to be updated later
-    }
-
-    return(SampleType$new(sample_type,
-      dilution_factor = dilution_factor,
-      validate_dilution = FALSE
-    ))
-  }
-
-  return(SampleType$new("TEST"))
+  return(sample_type)
 }
