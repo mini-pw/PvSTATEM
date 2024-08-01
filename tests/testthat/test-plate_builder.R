8a694ba2dd4a8fa28fbf02868c7e90b1ee02a19c
Mateusz Nizwantowski <mateusznizwantowski@gmail.com>
2024-08-01 15:13:21 +0200

    Name form luminex of layout file to sample type (#66)
    * temporary place to develop and test function to derive type and dilution
    
    * removed deriving dilutions in this function
    
    * Refactor derive_sample_type function to handle vector of strings
    
    * rename passed arguments to plural
    
    * insert new function implementation into code base
    
    * remove tmp file
    
    * tests of deriving type from names
    
    * positive control more generic recognition and compatibility with newer R versions

--- tests/testthat/test-plate_builder.R
+++ tests/testthat/test-plate_builder.R
@@ -3,3 +3,83 @@ test_that("Test convert dilutions to numeric", {
   dilution_values <- convert_dilutions_to_numeric(dilutions)
   expect_equal(dilution_values, c(NA, 0.02, 0.01))
 })
+
+
+# Test cases for translate_sample_names_to_sample_types function
+test_that("Sample type is correctly identified as BLANK", {
+  expect_equal(
+    translate_sample_names_to_sample_types(
+      c("B", "BLANK", "BACKGROUND"),
+      c("BLANK", "BACKGROUND", "B")
+    ),
+    c("BLANK", "BLANK", "BLANK")
+  )
+})
+
+test_that("Sample type is correctly identified as STANDARD CURVE", {
+  expect_equal(
+    translate_sample_names_to_sample_types(
+      c("S", "S 1/10", "S_1/100", "S1/1000"),
+      c("STANDARD CURVE", "1/10", "1/100", "1/1000")
+    ),
+    c("STANDARD CURVE", "STANDARD CURVE", "STANDARD CURVE", "STANDARD CURVE")
+  )
+})
+
+test_that("Sample type is correctly identified as NEGATIVE CONTROL", {
+  expect_equal(
+    translate_sample_names_to_sample_types(
+      c("NEGATIVE CONTROL", "N", "NEG"),
+      c("NEGATIVE CONTROL", "N", "NEGATIVE")
+    ),
+    c("NEGATIVE CONTROL", "NEGATIVE CONTROL", "NEGATIVE CONTROL")
+  )
+})
+
+test_that("Sample type is correctly identified as POSITIVE CONTROL", {
+  expect_equal(
+    translate_sample_names_to_sample_types(
+      c("P 1/100", "POS 1/50", "B770 1/2", "10/198 1/3", "Sample1", "Sample2", "Sample3"),
+      c("POSITIVE CONTROL", "", "B770", "10/198", "B770 1/2", "10/198 1/3", "C71/4 1/3")
+    ),
+    c("POSITIVE CONTROL", "POSITIVE CONTROL", "POSITIVE CONTROL", "POSITIVE CONTROL", "POSITIVE CONTROL", "POSITIVE CONTROL", "POSITIVE CONTROL")
+  )
+})
+
+test_that("Sample type defaults to TEST when no special conditions are met", {
+  expect_equal(
+    translate_sample_names_to_sample_types(
+      c("TEST1", "Unknown Sample", "SampleX"),
+      c("Different Name", "Another Name", "")
+    ),
+    c("TEST", "TEST", "TEST")
+  )
+})
+
+test_that("Handling of missing layout names", {
+  expect_equal(
+    translate_sample_names_to_sample_types(
+      c("BLANK", "S", "POS 1/10", "NEGATIVE CONTROL"),
+      NULL
+    ),
+    c("BLANK", "STANDARD CURVE", "POSITIVE CONTROL", "NEGATIVE CONTROL")
+  )
+
+  expect_equal(
+    translate_sample_names_to_sample_types(
+      c("BLANK", "S", "POS 1/10", "NEGATIVE CONTROL"),
+      NA
+    ),
+    c("BLANK", "STANDARD CURVE", "POSITIVE CONTROL", "NEGATIVE CONTROL")
+  )
+})
+
+test_that("Handling of empty layout names", {
+  expect_equal(
+    translate_sample_names_to_sample_types(
+      c("BLANK", "S", "POS 1/10", "NEGATIVE CONTROL"),
+      rep("", 4)
+    ),
+    c("BLANK", "STANDARD CURVE", "POSITIVE CONTROL", "NEGATIVE CONTROL")
+  )
+})
