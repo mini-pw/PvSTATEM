76bae8b1f100195e7c3a63a407ae90445733bede
Tymoteusz Kwieciński <31191783+Fersoil@users.noreply.github.com>
2024-09-05 10:13:20 +0200

    added unit test for incorrect layout path (#131)
    Co-authored-by: Fersoil <Fersoil>

--- tests/testthat/test-parser-layout.R
+++ tests/testthat/test-parser-layout.R
@@ -1,5 +1,10 @@
 library(testthat)
 
+test_that("Empty file triggers an error", {
+  path <- "incorrect_filepath.csv"
+  expect_error(read_layout_data(path, separator = ","))
+})
+
 test_that("Test reading layout from CSV file", {
   path <- system.file("extdata", "CovidOISExPONTENT_layout.csv", package = "PvSTATEM", mustWork = TRUE)
   layout_data <- read_layout_data(path, separator = ",")
