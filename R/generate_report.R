52c017f155e99e8c4020c6ee4475ec6e6ec2afcf
nizwant <mateusznizwantowski@gmail.com>
2024-09-19 16:15:51 +0200

    generate_report function

---
+++ R/generate_report.R
@@ -0,0 +1,13 @@
+#' Generate a report for a plate.
+#'
+#' This function generates a report for a plate. The report is generated using
+#' the `html_report_template.Rmd` template.
+#'
+#' @param plate A plate object.
+#' @param use_model A logical value indicating whether the model should be used in the report.
+#' @return A report.
+#' @export
+generate_report <- function(plate, use_model=TRUE) {
+  message("Generating report... This will take approximately 30 seconds.")
+  rmarkdown::render("R/html_report_template.Rmd", params = list(plate = plate, use_model = use_model))
+}
