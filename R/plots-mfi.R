ce3e1097bd202a8f6551ebf7cc15cd9bac9bd5f8
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2024-10-07 19:23:40 +0200

    Fix docs

--- R/plots-mfi.R
+++ R/plots-mfi.R
@@ -3,8 +3,14 @@
 #' @param plate A plate object
 #' @param analyte_name The analyte to plot
 #' @param data_type The type of data to plot. Default is "Median"
-#' @param plot_type The type of plot to generate. Default is "boxplot".
+#' @param plot_type The type of plot to generate. Default is "violin".
 #' Available options are "boxplot" and "violin".
+#' @param scale_y What kind of transformation of the scale to apply.
+#' By default MFI is presented in a log10 scale
+#' @param plot_outliers When using "boxplot" type of a plot
+#' one can set this parameter to TRUE and display the names of samples for
+#' which MFI falls outside the 1.5 IQR interval
+#'
 #'
 #' @return A ggplot object
 #'
