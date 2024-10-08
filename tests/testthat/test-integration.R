4572974a8f70e3a1ca9d4f1f7f2f64cbf51c5734
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2024-10-08 08:46:16 +0200

    Organize tests and add one more

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -1,24 +1,26 @@
 library(testthat)
 
 test_that("Fully Parse CovidOISExPONTENT.csv plate data", {
+  # Read plate
   path <- system.file("extdata", "CovidOISExPONTENT.csv", package = "PvSTATEM", mustWork = TRUE)
   expect_no_error(plate <- read_luminex_data(path, format = "xPONENT", verbose = FALSE))
-
-  expect_no_error(p <- plot_mfi_for_analyte(plate, "S2"))
-  expect_true(inherits(p, "ggplot"))
-
-  plate <- plate$blank_adjustment(in_place = FALSE)
-  expect_equal(plate$blank_adjusted, TRUE)
-
+  # Check basic properties
+  expect_equal(plate$plate_name, "CovidOISExPONTENT")
   expect_equal(plate$sample_names[10:13], c("S", "S", "S", "Unknown2"))
   expect_equal(plate$sample_types[10:13], c("STANDARD CURVE", "STANDARD CURVE", "STANDARD CURVE", "TEST"))
   expect_equal(plate$sample_types[1], "BLANK")
   expect_true(all(is.na(plate$dilutions)))
-
-  expect_equal(plate$plate_name, "CovidOISExPONTENT")
+  # Check plotting mfi for analyte
+  expect_no_error(p <- plot_mfi_for_analyte(plate, "S2"))
+  expect_true(inherits(p, "ggplot"))
+  expect_no_error(p <- plot_mfi_for_analyte(plate, "S2", plot_type = "boxplot", plot_outliers = TRUE))
+  expect_true(inherits(p, "ggplot"))
+  # Check blank adjustment
+  plate <- plate$blank_adjustment(in_place = FALSE)
+  expect_equal(plate$blank_adjusted, TRUE)
 })
 
-test_that("Validation works", {
+test_that("Test reading with layout", {
   path <- system.file("extdata", "random_no_standard_curve.csv", package = "PvSTATEM", mustWork = TRUE)
   layout_path <- system.file("extdata", "random_layout_no_standard_curve.xlsx", package = "PvSTATEM", mustWork = TRUE)
 
@@ -26,20 +28,19 @@ test_that("Validation works", {
 })
 
 test_that("Fully Parse CovidOISExPONTENT_CO.csv plate data with layout", {
+  # Read plate
   path <- system.file("extdata", "CovidOISExPONTENT.csv", package = "PvSTATEM", mustWork = TRUE)
   layout_path <- system.file("extdata", "CovidOISExPONTENT_layout.xlsx", package = "PvSTATEM", mustWork = TRUE)
   expect_no_error(plate <- read_luminex_data(path, format = "xPONENT", layout_filepath = layout_path, verbose = FALSE))
-
-  expect_equal(plate$dilutions[1:4], c(NA, "1/50", "1/100", "1/200"))
-
+  # Check basic properties
   expect_equal(plate$plate_name, "CovidOISExPONTENT")
-
+  expect_equal(plate$dilutions[1:4], c(NA, "1/50", "1/100", "1/200"))
+  # Check S3 functions
   expect_no_error(print(plate))
   expect_no_error(summary(plate))
-
+  # Test processing of a plate
   tmp_dir <- tempdir(check = TRUE)
   test_output_path <- file.path(tmp_dir, "output.csv")
-
   expect_no_error(
     process_plate(plate, output_path = test_output_path)
   )
