30c35e7e02af1059401916d4ca9caef4c8cd4d41
nizwant <mateusznizwantowski@gmail.com>
2024-10-10 20:08:56 +0200

    adding control over thresholds in counts plot

--- R/generate_report.R
+++ R/generate_report.R
@@ -5,14 +5,15 @@
 #'
 #' @param plate A plate object.
 #' @param use_model (`logical(1)`) A logical value indicating whether the model should be used in the report.
-#' @param filename (`character(1)`) The name of the output file. If not provided, the filename will be created
-#' based on the plate name with the suffix '_report.html'.
+#' @param filename (`character(1)`) The name of the output file. If not provided,the filename will be created based on the plate name with the suffix '_report.html'.
 #' @param output_dir (`character(1)`) The directory where the report should be saved. Default is 'reports'.
+#' @param counts_lower_threshold (`numeric(1)`) The lower threshold for the counts plots (works for each analyte). Default is 50.
+#' @param counts_higher_threshold (`numeric(1)`) The higher threshold for the counts plots (works for each analyte). Default is 70.
 #'
 #'
 #' @return A report.
 #' @export
-generate_plate_report <- function(plate, use_model = TRUE, filename = NULL, output_dir = "reports") {
+generate_plate_report <- function(plate, use_model = TRUE, filename = NULL, output_dir = "reports", counts_lower_threshold = 50, counts_higher_threshold = 70) {
   message("Generating report... This will take approximately 30 seconds.")
   output_file <- if (is.null(filename)) {
     paste0(plate$plate_name, "_report.html")
@@ -21,10 +22,10 @@ generate_plate_report <- function(plate, use_model = TRUE, filename = NULL, outp
   }
   rmarkdown::render(
     "R/plate_report_template.Rmd",
-    params = list(plate = plate, use_model = use_model),
+    params = list(plate = plate, use_model = use_model, counts_lower_threshold = counts_lower_threshold, counts_higher_threshold = counts_higher_threshold),
     output_file = output_file,
     output_dir = output_dir,
-    quiet=TRUE
+    quiet = TRUE
   )
   message(paste0("Report successfully generated, saving to: ", output_dir, "/", output_file))
 }
@@ -32,8 +33,8 @@ generate_plate_report <- function(plate, use_model = TRUE, filename = NULL, outp
 
 #' Generate a report with Levy-Jennings plots.
 #'
-#' This function generates a report with Levy-Jennings plots. The report is generated using
-#' the `levy_jennings_report_template.Rmd` template.
+#' This function generates a report with Levy-Jennings plots. The report
+#' is generated using the `levy_jennings_report_template.Rmd` template.
 #'
 #' @param list_of_plates A list of plate objects.
 #' @param filename (`character(1)`) The name of the output file. If not provided, the filename will be created
@@ -46,7 +47,7 @@ generate_plate_report <- function(plate, use_model = TRUE, filename = NULL, outp
 generate_levy_jennings_report <- function(list_of_plates, filename = NULL, output_dir = "reports") {
   message("Generating report... This will take approximately 30 seconds.")
   output_file <- if (is.null(filename)) {
-    paste0("levy_jenning_report.html") #### change this part
+    paste0("levy_jennings_report.html") #### change this part
   } else {
     filename
   }
@@ -55,7 +56,7 @@ generate_levy_jennings_report <- function(list_of_plates, filename = NULL, outpu
     params = list(list_of_plates = list_of_plates),
     output_file = output_file,
     output_dir = output_dir,
-    quiet=TRUE
+    quiet = TRUE
   )
   message(paste0("Report successfully generated, saving to: ", output_dir, "/", output_file))
 }
