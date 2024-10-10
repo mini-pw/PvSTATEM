4ea19a089c177a34951ded463f80c8facd6deaa5
nizwant <mateusznizwantowski@gmail.com>
2024-10-10 18:05:57 +0200

    adding the other missing arguments in docs

--- R/plots-standard_curve.R
+++ R/plots-standard_curve.R
@@ -12,6 +12,7 @@
 #' @param plot_line If `TRUE` a line is plotted, `TRUE` by default
 #' @param plot_blank_mean If `TRUE` the mean of the blank samples is plotted, `TRUE` by default
 #' @param plot_dilution_bounds If `TRUE` the dilution bounds are plotted, `TRUE` by default
+#' @param plot_legend If `TRUE` the legend is plotted, `TRUE` by default
 #' @param verbose If `TRUE` prints messages, `TRUE` by default
 #'
 #' @return ggplot object with the plot
