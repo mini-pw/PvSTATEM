1788c989294e8f1e8dbdd23f0ffab216fa7dcf91
ZetrextJG <jakubzgrzywaczewski@gmail.com>
2024-10-11 15:07:23 +0200

    Modify process_plate docs

--- R/process-plate.R
+++ R/process-plate.R
@@ -4,16 +4,16 @@
 #' The behavior can be summarized as follows:
 #' 1. Adjust blanks if not already done.
 #' 2. Fit a model to each analyte using standard curve samples.
-#' 3. Compute dilutions for each analyte using the corresponding model.
-#' 4. Aggregate computed dilutions into a single data frame.
-#' 5. Save the computed dilutions to a CSV file.
+#' 3. Predict RAU value for each analyte using the corresponding model.
+#' 4. Aggregate computed RAU values into a single data frame.
+#' 5. Save that data frame to a CSV file.
 #'
 #' @param plate (`Plate()`) a plate object
-#' @param output_path (`character(1)`) path to save the computed dilutions.
-#' If not provided the file will be saved in the working directory with the name `dilutions_{plate_name}.csv`.
+#' @param output_path (`character(1)`) path to save the computed RAU values
+#' If not provided the file will be saved in the working directory with the name `RAU_{plate_name}.csv`.
 #' Where the `{plate_name}` is the name of the plate.
 #' @param data_type (`character(1)`) type of data to use for the computation. Median is the default
-#' @param adjust_blanks (`logical(1)`) adjust blanks before computing dilutions. Default is `FALSE`
+#' @param adjust_blanks (`logical(1)`) adjust blanks before computing RAU values. Default is `FALSE`
 #' @param verbose (`logical(1)`) print additional information. Default is `TRUE`
 #' @param ... Additional arguments to be passed to the fit model function (`create_standard_curve_model_analyte`)
 #'
