5a13fa936dfeeaa68b3712bcc6a1eca461d21cc1
nizwant <mateusznizwantowski@gmail.com>
2024-12-22 16:34:10 +0100

    add new arguments to function

--- R/plots-levey_jennings.R
+++ R/plots-levey_jennings.R
@@ -1,19 +1,26 @@
 #' @title Plot Levey-Jennings chart
 #'
 #' @description
-#' The function plots a Levey-Jennings chart for the given analyte in the list of plates.
-#' The Levey-Jennings chart is a graphical representation of the data that enables the detection of outliers and trends.
-#' It is a quality control tool that is widely used in the laboratories across the world.
+#' The function plots a Levey-Jennings chart for the given analyte
+#' in the list of plates. The Levey-Jennings chart is a graphical
+#' representation of the data that enables the detection of outliers
+#' and trends. It is a quality control tool that is widely used
+#' in the laboratories across the world.
 #'
 #'
 #'
 #' @param list_of_plates  A list of plate objects for which to plot the Levey-Jennings chart
 #' @param analyte_name (`character(1)`) the analyte for which to plot the Levey-Jennings chart
+#' @param sd_lines (`numeric`) the vector of coefficients for the
+#' standard deviation lines to plot for example c(1.96, 2.58)
+#' will plot four horizontal lines: mean +/- 1.96*sd, mean +/- 2.58*sd
+#' default is c(1.96) which will plot two lines mean +/- 1.96*sd
+#' @param data_type (`character(1)`) the type of data used plot. Default is "Median"
 #'
 #' @return A ggplot object with the Levey-Jennings chart
 #'
 #' @export
-plot_levey_jennings <- function(list_of_plates, analyte_name) {
+plot_levey_jennings <- function(list_of_plates, analyte_name, sd_lines = c(1.96), data_type="Median") {
   stopifnot(is.list(list_of_plates))
   stopifnot(length(list_of_plates) > 0)
   stopifnot(all(sapply(list_of_plates, inherits, "Plate")))
@@ -21,5 +28,5 @@ plot_levey_jennings <- function(list_of_plates, analyte_name) {
   stopifnot(is.character(analyte_name))
   stopifnot(all(sapply(list_of_plates, function(plate) analyte_name %in% plate$analyte_names)))
 
-
+  
 }
