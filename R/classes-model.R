92b30e8d47eb3595e015dfe9d76afdc2a2199e74
Jakub Grzywaczewski <44850303+ZetrextJG@users.noreply.github.com>
2025-01-13 13:40:35 +0100

    docs: fix examples docs string (#232)

--- R/classes-model.R
+++ R/classes-model.R
@@ -386,7 +386,7 @@ create_standard_curve_model_analyte <- function(plate, analyte_name,
 #' @param high_dose_threshold (`numeric(1)`) MFI values associated
 #' with dilutions above this threshold should be checked for the high dose hook effect
 #'
-#' @example
+#' @examples
 #' plate_filepath <- system.file("extdata", "CovidOISExPONTENT.csv", package = "PvSTATEM", mustWork = TRUE) # get the filepath of the csv dataset
 #' layout_filepath <- system.file("extdata", "CovidOISExPONTENT_layout.xlsx", package = "PvSTATEM", mustWork = TRUE)
 #' plate <- read_luminex_data(plate_filepath, layout_filepath) # read the data
@@ -396,7 +396,6 @@ create_standard_curve_model_analyte <- function(plate, analyte_name,
 #'
 #' # here we create the model with the high dose hook effect handled
 #' model <- create_standard_curve_model_analyte(plate, "RBD_omicron")
-#'
 #'
 #' @return sample selector (`logical()`)
 handle_high_dose_hook <- function(mfi, dilutions, high_dose_threshold = 1 / 200) {
