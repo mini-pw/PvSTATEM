9527007c0ed8ae9391a2de2c3f4616fab1b51d0c
Fersoil <Fersoil>
2025-03-11 10:48:17 +0100

    update the links to docs

--- R/classes-plate.R
+++ R/classes-plate.R
@@ -172,8 +172,8 @@ Plate <- R6::R6Class(
     #'  The possible values are \cr \code{c(`r toString(VALID_SAMPLE_TYPES)`)}.
     #'  Sample types are ordered in the same way as the `sample_names` vector.
     #'
-    #'  If the Plate object is initialised using the default methods ( \code{\link=read_luminex_data]{read_luminex_data}}
-    #'  or any of the processing methods: \code{\link=process_dir]{process_dir}}, \code{\link=process_file]{process_file}} and \code{\link=process_plate]{process_plate}})
+    #'  If the Plate object is initialised using the default methods ( \code{\link[=read_luminex_data]{read_luminex_data}}
+    #'  or any of the processing methods: \code{\link[=process_dir]{process_dir}}, \code{\link[=process_file]{process_file}} and \code{\link[=process_plate]{process_plate}})
     #'  the sample types are detected based on the sample names according to the rules described in \code{\link[=translate_sample_names_to_sample_types]{translate_sample_names_to_sample_types}}.
     #'
     #'
